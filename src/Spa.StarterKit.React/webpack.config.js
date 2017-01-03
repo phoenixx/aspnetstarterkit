eb9a8b7a6d9dee6e1df0e091982776ec9d4c461b
Michael Rose <mik.rose@outlook.com>
2017-01-03 16:22:16 +0000

    webpack.config rewrite WIP

--- src/Spa.StarterKit.React/webpack.config.js
+++ src/Spa.StarterKit.React/webpack.config.js
@@ -1,48 +1,60 @@
-﻿var isDevBuild = process.argv.indexOf('--env.prod') < 0;
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var merge = require('webpack-merge');
-var copyWebpackPlugin = require('copy-webpack-plugin');
+﻿let isDevBuild = process.argv.indexOf('--env.prod') < 0;
+const extractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const merge = require('webpack-merge');
+const autoprefixer = require('autoprefixer');
+const clientBundleOutputDir = './wwwroot/dist';
+const webpack = require('webpack');
+const copyWebpackPlugin = require('copy-webpack-plugin');
 
-// Configuration in common to both client-side and server-side bundles
-var sharedConfig = () => ({
+const sharedConfig = () => ({
     resolve: { extensions: ['', '.js', '.jsx'] },
     output: {
         filename: '[name].js',
-        publicPath: '/dist/' // Webpack dev middleware, if enabled, handles requests for this URL prefix
+        publicPath: '/dist/'
     },
     module: {
         loaders: [
-            { test: /\.jsx?$/, include: /Client/, loader: 'babel-loader' }
+        {
+            test: /\.jsx$/,
+            include: /Client/,
+            loader: 'babel-loader',
+            exclude: /(node_modules|bower_components)/
+
+        }
         ]
     }
 });
 
-// Configuration for client-side bundle suitable for running in browsers
-var clientBundleOutputDir = './wwwroot/dist';
-var clientBundleConfig = merge(sharedConfig(), {
+const clientBundleConfig = merge(sharedConfig(),
+{
+    //context: path.join(__dirname, 'Client'),
     entry: {
-        'main-client': [
-            './Client/Index.jsx'
-        ]
+                'main-client': [
+                    './Client/Index.jsx'
+                ]
+            },
+    output: {
+        path: path.join(__dirname, 'Client', 'Dist'),
+        filename: '[name].js'
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: ExtractTextPlugin.extract(['css-loader']) },
+            { test: /\.css$/, loader: 'style!css' },
             { test: /\.(png|jpg|jpeg|gif|svg)$/, loader: 'url-loader', query: { limit: 25000 } },
             {
                 test: /\.scss$/,
-                loaders: ['style-loader', 'css-loader', 'sass-loader']
+                loaders: ['style', 'css', 'postcss', 'sass']
             }
         ]
     },
-    sassLoader: {
-        includePaths: [path.resolve(__dirname, './sass')]
+    postcss: function () {
+        return [autoprefixer({
+            browsers: ['last 3 versions']
+        })];
     },
-    output: { path: path.join(__dirname, clientBundleOutputDir) },
     plugins: [
-        new ExtractTextPlugin('site.css'),
+        new extractTextPlugin('site.css'),
         new webpack.DllReferencePlugin({
             context: __dirname,
             manifest: require('./wwwroot/dist/vendor-manifest.json')
@@ -63,24 +75,139 @@ var clientBundleConfig = merge(sharedConfig(), {
     ])
 });
 
-// Configuration for server-side (prerendering) bundle suitable for running in Node
-var serverBundleConfig = merge(sharedConfig(), {
-    resolve: { packageMains: ['main'] },
-    entry: { 'main-server': './Client/index.jsx' },
-    plugins: [
-        new webpack.DllReferencePlugin({
-            context: __dirname,
-            manifest: require('./Client/dist/vendor-manifest.json'),
-            sourceType: 'commonjs2',
-            name: './vendor'
-        })
-    ],
-    output: {
-        libraryTarget: 'commonjs',
-        path: path.join(__dirname, './Client/dist')
-    },
-    target: 'node',
-    devtool: 'inline-source-map'
-});
+//const serverBundleConfig = merge(sharedConfig(), {
+//    resolve: {
+//         packageMains: ['main']
+//    },
+//    module: {
+//        loaders: [
+//          {
+//              test: /\.jsx$/,
+//              include: /Client/,
+//              loader: 'babel-loader',
+//              exclude: /(node_modules|bower_components)/
 
-module.exports = [clientBundleConfig, serverBundleConfig];
+//          },
+//        {
+//            test: /\.scss$/,
+//            loaders: ['style', 'css', 'postcss', 'sass']
+//        }]
+//    },
+//    entry: {
+//         'main-server': './Client/index.jsx'
+//    },
+//    plugins: [
+//        new webpack.DllReferencePlugin({
+//            context: __dirname,
+//            manifest: require('./Client/dist/vendor-manifest.json'),
+//            sourceType: 'commonjs2',
+//            name: './vendor'
+//        })
+//    ],
+//    output: {
+//        libraryTarget: 'commonjs',
+//        path: path.join(__dirname, './Client/dist')
+//    },
+//    target: 'node',
+//    devtool: 'inline-source-map'
+//});
+
+module.exports = [clientBundleConfig];//, serverBundleConfig];
+
+//module.exports = {
+//    context: path.join(__dirname, 'Client'),
+//    entry: './Index.jsx',
+//    output: {
+//        path: path.join(__dirname, 'Client', 'Dist'),
+//        filename: '[name].bundle.js'
+//    }
+//};
+
+
+
+//var isDevBuild = process.argv.indexOf('--env.prod') < 0;
+//var path = require('path');
+//var webpack = require('webpack');
+//var ExtractTextPlugin = require('extract-text-webpack-plugin');
+//var merge = require('webpack-merge');
+//var copyWebpackPlugin = require('copy-webpack-plugin');
+
+//// Configuration in common to both client-side and server-side bundles
+//var sharedConfig = () => ({
+//    resolve: { extensions: ['', '.js', '.jsx'] },
+//    output: {
+//        filename: '[name].js',
+//        publicPath: '/dist/' // Webpack dev middleware, if enabled, handles requests for this URL prefix
+//    },
+//    module: {
+//        loaders: [
+//            { test: /\.jsx?$/, include: /Client/, loader: 'babel-loader', exclude: /(node_modules|bower_components)/ }
+//        ]
+//    }
+//});
+
+//// Configuration for client-side bundle suitable for running in browsers
+//var clientBundleOutputDir = './wwwroot/dist';
+//var clientBundleConfig = merge(sharedConfig(), {
+//    entry: {
+//        'main-client': [
+//            './Client/Index.jsx'
+//        ]
+//    },
+//    module: {
+//        loaders: [
+//            { test: /\.css$/, loader: ExtractTextPlugin.extract(['css-loader']) },
+//            { test: /\.(png|jpg|jpeg|gif|svg)$/, loader: 'url-loader', query: { limit: 25000 } },
+//            {
+//                                test: /\.scss$/,
+//                                loaders: ['style', 'css', 'postcss', 'sass']
+//            }
+//        ]
+//    },
+//    sassLoader: {
+//        includePaths: [path.resolve(__dirname, './sass')]
+//    },
+//    output: { path: path.join(__dirname, clientBundleOutputDir) },
+//    plugins: [
+//        new ExtractTextPlugin('site.css'),
+//        new webpack.DllReferencePlugin({
+//            context: __dirname,
+//            manifest: require('./wwwroot/dist/vendor-manifest.json')
+//        }),
+//        new copyWebpackPlugin([
+//            { from: 'Client/images', to: 'images' }
+//        ])
+//    ].concat(isDevBuild ? [
+//        // Plugins that apply in development builds only
+//        new webpack.SourceMapDevToolPlugin({
+//            filename: '[file].map', // Remove this line if you prefer inline source maps
+//            moduleFilenameTemplate: path.relative(clientBundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
+//        })
+//    ] : [
+//        // Plugins that apply in production builds only
+//        new webpack.optimize.OccurenceOrderPlugin(),
+//        new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
+//    ])
+//});
+
+//// Configuration for server-side (prerendering) bundle suitable for running in Node
+//var serverBundleConfig = merge(sharedConfig(), {
+//    resolve: { packageMains: ['main'] },
+//    entry: { 'main-server': './Client/index.jsx' },
+//    plugins: [
+//        new webpack.DllReferencePlugin({
+//            context: __dirname,
+//            manifest: require('./Client/dist/vendor-manifest.json'),
+//            sourceType: 'commonjs2',
+//            name: './vendor'
+//        })
+//    ],
+//    output: {
+//        libraryTarget: 'commonjs',
+//        path: path.join(__dirname, './Client/dist')
+//    },
+//    target: 'node',
+//    devtool: 'inline-source-map'
+//});
+
+//module.exports = [clientBundleConfig, serverBundleConfig];

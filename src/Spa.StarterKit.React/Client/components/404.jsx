dfae99b3eb857d7067d832295ec4ac513af61d39
phoenixx <mik.rose@outlook.com>
2017-01-16 14:40:51 +0000

    Added client-side 404 handler

---
+++ src/Spa.StarterKit.React/Client/components/404.jsx
@@ -0,0 +1,21 @@
+﻿import React, { Component } from 'react';
+import '../sass/404.scss';
+
+class RouteNotFound extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const attemptedRoute = this.props.params.splat;
+        return(
+            <div className="notfound--container">
+                Dude, that page no existio
+                <div className="notfound--subtext">
+                    Page '{attemptedRoute}' is not currently in existence.
+                </div>
+            </div>
+        );
+    }
+}
+
+export default RouteNotFound;
